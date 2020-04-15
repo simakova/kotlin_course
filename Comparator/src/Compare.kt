eb0716410a6fc0567ccc202b730746a326add531
Ksenia <Ksenia.Simakova@x5.ru>
2020-04-15 13:35:27 +0300

    task6

---
+++ Comparator/src/Compare.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>){
+    print("Введите 1-е число: ")
+    var first:String? = readLine()
+    print("Введите 2-е число: ")
+    var second:String? = readLine()
+    var firstNum = first!!.toDouble()
+    var secondNum = second!!.toDouble()
+    var boolVar = firstNum>secondNum
+    println(boolVar)
+
+
+}
