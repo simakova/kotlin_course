e69cc3d46e6e081c9e28b15deea4dfef3236d148
Ksenia <Ksenia.Simakova@x5.ru>
2020-04-15 12:50:33 +0300

    task 3

---
+++ Conversion/src/Conversion.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var five: Char = '5'
+    println(five.toByte())
+    println(five.toShort())
+    println(five.toInt())
+    println(five.toFloat())
+    println(five.toDouble())
+    println(five.toLong())
+    println(five.toString())
+}
