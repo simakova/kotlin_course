efa91d082fc3552db39b9918f1ce55180071c08f
Ksenia <Ksenia.Simakova@x5.ru>
2020-04-15 13:03:37 +0300

    task4

---
+++ Readline/src/Run.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>){
+    print("Введите свое имя: ")
+    var usertext: String? = readLine()
+    var name: String = usertext.toString()
+    println("Приветствую тебя, $name!")
+}
