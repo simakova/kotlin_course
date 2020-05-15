5e66cd4e7db47e60511283ffde46fc4795a0862f
Ksenia <Ksenia.Simakova@x5.ru>
2020-05-15 23:29:36 +0300

    add 4_17 task

---
+++ 4_17_Aircompany/src/Aircraft.kt
@@ -0,0 +1,13 @@
+class Aircraft (){
+    var id: Int = 1
+    var maxRange:Double = 500.0
+    var capacity:Double = 70.0
+    val gasMileage100: Double
+        get() = ((capacity.div(maxRange))*100)
+}
+
+fun main() {
+    val airplane: Aircraft = Aircraft()
+
+    print(airplane.gasMileage100)
+}
